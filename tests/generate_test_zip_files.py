2e688747e0da6fc97735cf7ffce7a6ca98626fea
Tiffany Timbers <tiffany.timbers@gmail.com>
2023-11-23 20:35:57 -0800

    fixing Dockerfile and removing excel file type dependency as it wasn't really needed

--- tests/generate_test_zip_files.py
+++ tests/generate_test_zip_files.py
@@ -1,6 +1,4 @@
-import pytest
 import os
-import openpyxl
 import zipfile
 import shutil
 
@@ -11,32 +9,31 @@ os.makedirs('subdir', exist_ok=True)
 with open('test1.txt', 'w') as file:
     file.write('test data')
 
-# Create 'test2.txt' inside 'subdir' and write "test data" to it
-with open('subdir/test2.txt', 'w') as file:
+# Create 'test2.csv' and write "test data" to it
+with open('test2.csv', 'w') as file:
+    file.write('test,data')
+
+# Create 'test3.txt' inside 'subdir' and write "test data" to it
+with open('subdir/test3.txt', 'w') as file:
     file.write('test data')
 
-# Create 'test1.xlsx' and write "test data" to it
-workbook = openpyxl.Workbook()
-worksheet = workbook.active
-worksheet.cell(row=1, column=1, value='test data')
-workbook.save('test1.xlsx')
-
-# Case 1 - Create a zip file containing 'test1.txt' and 'test1.xlsx'
-with zipfile.ZipFile('files_txt_xlsx.zip', 'w', zipfile.ZIP_DEFLATED) as zipf:
+# Case 1 - Create a zip file containing 'test1.txt' and 'test2.csv'
+with zipfile.ZipFile('files_txt_csv.zip', 'w', zipfile.ZIP_DEFLATED) as zipf:
     zipf.write('test1.txt')
-    zipf.write('test1.xlsx')
+    zipf.write('test2.csv')
 
-# Case 2 - Create a zip file containing 'test1.txt' and 'subdir/test2.txt'
+# Case 2 - Create a zip file containing 'test1.txt', test2.csv and 'subdir/test2.txt'
 with zipfile.ZipFile('files_txt_subdir.zip', 'w', zipfile.ZIP_DEFLATED) as zipf:
     zipf.write('test1.txt')
-    zipf.write('subdir/test2.txt')
+    zipf.write('test2.csv')
+    zipf.write('subdir/test3.txt')
 
 # Case 3 - Create an empty zip file
 with zipfile.ZipFile('empty.zip', 'w', zipfile.ZIP_DEFLATED) as zipf:
     pass
 
 # Clean up the files and directories created
-test_files = ['test1.txt', 'test1.xlsx']
+test_files = ['test1.txt', 'test2.csv']
 
 for file in test_files:
     if os.path.exists(file):
