9b9930980f2ce507ee7c6e510c3c984cb076c6f3
Tiffany Timbers <tiffany.timbers@gmail.com>
2023-11-24 12:08:57 -0800

    added read_zip function call to notebook and improved exception handling in case the files were already zipped

--- src/read_zip.py
+++ src/read_zip.py
@@ -25,7 +25,8 @@ def read_zip(url, directory):
         raise ValueError('The URL provided does not exist.')
     
     # check if the URL points to a zip file, if not raise an error  
-    if request.headers['content-type'] != 'application/zip':
+    #if request.headers['content-type'] != 'application/zip':
+    if filename_from_url[-4:] != '.zip':
         raise ValueError('The URL provided does not point to a zip file.')
     
     # check if the directory exists, if not raise an error
@@ -39,6 +40,11 @@ def read_zip(url, directory):
 
     # get list of files/directories in the directory
     original_files = os.listdir(directory)
+    original_timestamps = []
+    for filename in original_files:
+        filename = os.path.join(directory, filename)
+        original_timestamp = os.path.getmtime(filename)
+        original_timestamps.append(original_timestamp)
 
     # extract the zip file to the directory
     with zipfile.ZipFile(path_to_zip_file, 'r') as zip_ref:
@@ -47,5 +53,10 @@ def read_zip(url, directory):
     # check if any files were extracted, if not raise an error
     # get list of files/directories in the directory
     current_files = os.listdir(directory)
-    if len(current_files) == len(original_files):
+    current_timestamps = []
+    for filename in current_files:
+        filename = os.path.join(directory, filename)
+        current_timestamp = os.path.getmtime(filename)
+        current_timestamps.append(current_timestamp)
+    if (len(current_files) == len(original_files)) & (original_timestamps == current_timestamps):
         raise ValueError('The ZIP file is empty.')
