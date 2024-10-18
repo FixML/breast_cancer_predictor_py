fee3c78a98bc6f3e05c1794c11e41a8f9472c2f0
hwl1008 <weilinhan1008@gmail.com>
2024-10-18 10:59:31 -0700

    adding clean_data.py and tests

---
+++ src/clean_data.py
@@ -0,0 +1,90 @@
+# clean_data.py
+# author: Weilin Han
+# date: 2024-10-15
+
+import pandas as pd
+import re
+import os
+
+
+def extract_column_name(raw_name_file):
+    """Extract and clean column names from .names file."""
+    
+    # Test 1: Ensure the raw name file exists, if not raise error
+    if not os.path.exists(raw_name_file):
+        raise FileNotFoundError(f"The raw name file does not exist.")
+    
+    # Test 2: Ensure the raw name file is a .names file, if not raise error
+    if not raw_name_file.endswith('.names'):
+        raise ValueError("The raw name file must be a '.names' file.")
+    
+    # Extracting column names from downloaded raw file
+    text_lines = []
+    with open(raw_name_file, 'r') as file:
+        for line in file:
+            line = line.strip()
+            if not line.startswith('#') and line:  # Skip comma
+                text_lines.append(line)
+        start = text_lines.index('7. Attribute information')
+        end = text_lines.index('8. Missing attribute values: none')
+        text_lines = text_lines[start:end]
+        
+        pattern = re.compile(r'^[1-9a-z]\)\s*')
+        text_lines = [item for item in text_lines if pattern.match(item)]
+        text_lines = [pattern.sub('', item) for item in text_lines]
+        text_lines = [item.split()[0].lower() for item in text_lines]
+
+        statistics = ['mean','se','max'] 
+        #se is standard error, and max is the worst or largest (mean of three largest values)
+
+        # please refer to original file for explanation of feactures
+        colnames = text_lines[0:2]
+        for stat in statistics:
+            for feature in text_lines[2:]:
+                colnames.append(stat+'_'+feature)
+        
+    return colnames
+    
+def read_raw_data(raw_data, col_name):
+    """Read data from .data file."""
+
+    # Test 1: Ensure the raw data file exists, if not raise error
+    if not os.path.exists(raw_data):
+        raise FileNotFoundError(f"The raw_data file does not exist.")
+    
+    # Test 2: Ensure the raw_data file's extension is .data', if not raise error
+    if not raw_data.endswith('.data'):
+        raise ValueError("The raw_data file must be a .data file.")
+    
+    # Test 3: Ensure the col_name is a list, if not raise error
+    if not isinstance(col_name, list):
+        raise ValueError("col_name must be a list.")
+    
+    # Test 4: Ensure the list has 32 items, if not raise error
+    if len(col_name) != 32:
+        raise ValueError("col_name must contain exactly 32 items.")
+    
+    # Test 5: Ensure the list only contains strings, if not raise error
+    if not all(isinstance(item, str) for item in col_name):
+        raise ValueError("col_name must only contain strings.")
+    
+    imported_data = pd.read_csv(raw_data, names=col_name, header=None)
+    return imported_data
+
+def clean_data(imported_data, drop_columns=['id'], relabel={'M' : 'Malignant','B' : 'Benign'}):
+    """Clean imported data"""
+    # Test 1: Ensure the imported_data is a dataframe
+    if not isinstance(imported_data, pd.DataFrame):
+        raise ValueError("imported_data must be a data frame.")
+    
+    # Test 2: Ensure the drop_columns is a list
+    if not isinstance(drop_columns, list):
+        raise ValueError("drop_columns must be a list.")
+    
+    # Test 3: Ensure the relabel is a dictionary
+    if not isinstance(relabel, dict):
+        raise ValueError("relabel must be a dictionary")
+    
+    cleaned_data = imported_data.drop(columns=drop_columns)
+    cleaned_data['class'] = cleaned_data['class'].replace(relabel)
+    return cleaned_data
---
+++ tests/test_clean_data.py
@@ -0,0 +1,95 @@
+import pytest
+import numpy as np
+import pandas as pd
+from click.testing import CliRunner
+from click import BadParameter
+import os
+from unittest.mock import patch, mock_open
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from src.clean_data import *
+
+# Test files setup
+col_name1 = ["col" + str(i) for i in range(32)] # 32 strings
+col_name2 = {"1":"apple"}
+col_name3 = ['1','2','3']
+col_name4 = ["col" + str(i) for i in range(31)] + [123]  # 31 strings + 1 integer
+
+imported_data1 = pd.DataFrame({
+        'id': [1, 2, 3],
+        'class': ['M', 'B', 'M']
+    })
+imported_data2 = [1, 2, 3, 4, 5]
+drop_columns1=['id']
+drop_columns2={'1':'id'}
+relabel1={'M' : 'Malignant','B' : 'Benign'}
+relabel2=['M','B']
+
+# Tests
+
+# Tests for extract_column_name
+
+# test extract_column_name function throws an error 
+# if the raw name file does not exist
+def test_extract_column_name_error_on_missing_file():
+    with pytest.raises(FileNotFoundError, match='The raw name file does not exist.'):
+        extract_column_name('tests/test_name_data.name')
+
+# test extract_column_name function throws an error 
+# if the raw name file is not a .names file
+def test_extract_column_name_error_on_wrong_file_type():
+    with pytest.raises(ValueError, match='The raw name file must be a .names file.'):
+        extract_column_name('tests/empty.zip')
+
+# Tests for read_raw_data
+
+# test read_raw_data function throws an error 
+# if the raw data file does not exist
+def test_read_raw_data_error_on_missing_file():
+    with pytest.raises(FileNotFoundError, match='The raw_data file does not exist.'):
+        read_raw_data('tests/test_raw_data.data',col_name1)
+
+# test read_raw_data function throws an error 
+# if the raw data file is not a .data file
+def test_read_raw_data_error_on_wrong_file_format():
+    with pytest.raises(ValueError, match='The raw data file must be a .names file.'):
+        read_raw_data('tests/empty.zip', col_name1)
+
+# test read_raw_data function throws an error 
+# if the col_name is not a list
+def test_read_raw_data_error_on_non_list():
+    with pytest.raises(ValueError, match="col_name must be a list."):
+        read_raw_data('tests/test_wdbc.data',col_name2)
+
+# test read_raw_data function throws an error 
+# if the col_name does not have 32 values
+def test_read_raw_data_error_on_insufficient_list_item():
+    with pytest.raises(ValueError, match="col_name must contain exactly 32 items."):
+        read_raw_data('tests/test_wdbc.data', col_name3)
+
+# test read_raw_data function throws an error 
+# if the col_name contains items other than string
+def test_read_raw_data_error_on_wrong_item_type():
+    with pytest.raises(ValueError, match="col_name must only contain strings."):
+        read_raw_data('tests/test_wdbc.data', col_name4)
+
+# Tests for clean_data
+
+# test clean_data function throws an error
+# if the imported_data is not a dataframe
+def test_clean_data_error_on_wrong_imported_data_format():
+    with pytest.raises(ValueError, match="imported_data must be a data frame."):
+        clean_data(imported_data2, drop_columns1, relabel1)
+
+# test clean_data function throws an error
+# if the drop_columns is not a list
+def test_clean_data_error_on_wrong_drop_columns_format():
+    with pytest.raises(ValueError, match="drop_columns must be a list."):
+        clean_data(imported_data1, drop_columns2, relabel1)
+
+
+# test clean_data function throws an error
+# if the relabel is not a dictionary
+def test_clean_data_error_on_wrong_relabel_format():
+    with pytest.raises(ValueError, match="relabel must be a dictionary"):
+        clean_data(imported_data1, drop_columns1, relabel2)
